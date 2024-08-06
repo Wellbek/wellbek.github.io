de86ff025408d95d27445746d5fba03c5fabf7ff
Wellbek <louis.wellmeyer@gmail.com>
2024-08-06 11:45:27 +0200

    Added reusable banner with button for chapters; Fixed custom cursor issues when scrolling

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const cursor = document.querySelector(".cursor");
 const links = document.querySelectorAll("a");
-const images = document.querySelectorAll("img");
+const zoomables = document.querySelectorAll(".zoomable");
+const pulse_buttons = document.querySelectorAll(".pulse")
 
 // Update cursor position based on mouse movement
 document.addEventListener("mousemove", (e) => {
     cursor.setAttribute(
         "style",
-        "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+        "top: " + (e.clientY - 10) + "px; left: " + (e.clientX - 10) + "px;"
     );
 });
 
@@ -21,14 +22,24 @@ links.forEach((link) => {
     });
 });
 
-images.forEach((img) => {
-    img.addEventListener("mouseenter", () => {
-        // cursor.classList.add("hovered"); // Add hovered class to enlarge cursor
-        img.style.transform = "scale(1.1)"
+zoomables.forEach((zoom) => {
+    zoom.addEventListener("mouseenter", () => {
+        zoom.style.transform = "scale(1.1)"
     });
 
-    img.addEventListener("mouseleave", () => {
-        // cursor.classList.remove("hovered"); // Remove hovered class to shrink cursor
-        img.style.transform = "scale(1)"
+    zoom.addEventListener("mouseleave", () => {
+        zoom.style.transform = "scale(1)"
+    });
+})
+
+pulse_buttons.forEach((button) => {
+    button.addEventListener("mouseenter", () => {
+        button.style.transform = "scale(1.2)"
+        button.classList.add("active");
+    });
+
+    button.addEventListener("mouseleave", () => {
+        button.style.transform = "scale(1)"
+        button.classList.remove("active");
     });
 })
